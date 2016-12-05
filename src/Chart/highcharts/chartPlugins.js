80bc5b1dd6172ce5c0e552d797072a0bc623a57d
Lukas Horak <info@lhorak.cz>
2016-12-05 15:54:44 +0100

    RELATED: CL-10949 CL-10952 Removed shortening of axis labels. Modified hiding of data labels

--- src/Chart/highcharts/chartPlugins.js
+++ src/Chart/highcharts/chartPlugins.js
@@ -1,81 +1,5 @@
-import { debounce } from 'lodash';
 import autohideLabels from './plugins/autohideLabels/autohideLabels';
 import { extendDataLabelColors } from './plugins/dataLabelsColors';
-
-const getUsableContWidth = (chart, chartTextPadding) =>
-    chart.renderTo.getBoundingClientRect().width - chartTextPadding;
-
-const resizeYAxisLabel = (axis, chartTextPadding) => {
-    if (axis.chart && axis.axisTitle) {
-        const chart = axis.chart;
-        const currentWidth = parseInt(axis.options.title.style.width.replace(/\D/, ''), 10);
-        const newWidth = chart.plotHeight - chartTextPadding;
-
-        if (currentWidth !== newWidth) {
-            const title = axis.options.title;
-            title.style.width = `${newWidth}px`;
-            axis.setTitle(title);
-        }
-    }
-};
-
-const extendInitChart = (Highcharts) => {
-    Highcharts.wrap(Highcharts.Chart.prototype, 'init', function(proceed, options, callback) { // eslint-disable-line
-        const chart = this;
-        const widthByContainer = `${getUsableContWidth(options.chart)}px`;
-
-        chart.resizeYAxisLabel = debounce(resizeYAxisLabel, 500);
-
-        proceed.call(chart, options, callback);
-
-        chart.oldContainerWidth = widthByContainer;
-    });
-};
-
-const extendInitAxis = (Highcharts, chartTextPadding) => {
-    Highcharts.wrap(Highcharts.Axis.prototype, 'init', function(proceed, chart, options) { // eslint-disable-line
-        chart.chartType = chart.options.chart.type; // eslint-disable-line
-
-        if (options.title) {
-            options.title.style.width = (options.title.rotation === 0) // eslint-disable-line
-                ? `${chart.plotWidth - chartTextPadding}px` // axis title is horizontal
-                : `${chart.plotHeight}px`; // axis title is vertical i.e. rotation === 270
-        }
-        proceed.call(this, chart, options);
-    });
-};
-
-const extendReflowChart = (Highcharts, chartTextPadding) => {
-    Highcharts.wrap(Highcharts.Chart.prototype, 'reflow', function(proceed, resizeEvent) { // eslint-disable-line
-        const chart = this;
-
-        Highcharts.each(chart.xAxis, (axis) => {
-            if (axis.options.title) {
-                const currentWidth = parseInt(axis.options.title.style.width.replace(/\D/, ''), 10);
-                const newWidth = chart.plotWidth - chartTextPadding;
-
-                if (currentWidth !== newWidth) {
-                    const title = axis.options.title;
-                    title.style.width = `${newWidth}px`;
-                    axis.setTitle(title);
-                }
-            }
-        });
-        Highcharts.each(chart.yAxis, (axis) => {
-            if (axis.options.title) {
-                chart.resizeYAxisLabel(axis, chartTextPadding);
-            }
-        });
-
-        proceed.call(chart, resizeEvent);
-
-        chart.oldContainerWidth = chart.oldContainerWidth || getUsableContWidth(chart);
-        if (chart.legend.itemStyle) {
-            chart.resizeLegend(chart, chartTextPadding);
-        }
-        chart.oldContainerWidth = getUsableContWidth(chart);
-    });
-};
 
 const extendRenderStackTotals = (Highcharts) => {
     Highcharts.wrap(Highcharts.Axis.prototype, 'renderStackTotals', function(proceed) { // eslint-disable-line
@@ -102,10 +26,7 @@ const extendRenderStackTotals = (Highcharts) => {
     });
 };
 
-export function initChartPlugins(Highcharts, chartTextPadding) {
-    extendInitChart(Highcharts, chartTextPadding);
-    extendInitAxis(Highcharts, chartTextPadding);
-    extendReflowChart(Highcharts, chartTextPadding);
+export function initChartPlugins(Highcharts) {
     extendRenderStackTotals(Highcharts);
     autohideLabels(Highcharts);
     extendDataLabelColors(Highcharts);
--- src/Chart/highcharts/plugins/autohideLabels/autohideLabels.js
+++ src/Chart/highcharts/plugins/autohideLabels/autohideLabels.js
@@ -1,29 +1,22 @@
-import { debounce } from 'lodash';
-
-import { getChartType, hideAllLabels } from '../../helpers';
+import { getChartType } from '../../helpers';
 import { COLUMN_CHART, BAR_CHART } from '../../../../VisualizationTypes';
 import autohideColumnLabels from './autohideColumnLabels';
 import autohideBarLabels from './autohideBarLabels';
 
 const autohideLabels = (Highcharts) => {
-    Highcharts.Chart.prototype.callbacks.push((chart) => {
-        const chartType = getChartType(chart);
-        const reformatLabels = () => {
-            if (chartType === COLUMN_CHART) {
-                autohideColumnLabels(chart);
-            }
-            if (chartType === BAR_CHART) {
-                autohideBarLabels(chart);
-            }
-        };
-        hideAllLabels(chart);
-        reformatLabels();
+    Highcharts.wrap(Highcharts.Chart.prototype, 'hideOverlappingLabels', function(proceed, labels) { // eslint-disable-line
+        const chart = this;
+        const chartType = getChartType(this);
 
-        /*
-         Highcharts fires 'redraw' event several times for one change, we try here
-         to reduce number of calls of 'reformatLabels' for better performance.
-         */
-        Highcharts.addEvent(chart, 'redraw', debounce(reformatLabels, 500));
+        if (chartType === COLUMN_CHART) {
+            autohideColumnLabels(chart);
+            return;
+        }
+        if (chartType === BAR_CHART) {
+            autohideBarLabels(chart);
+            return;
+        }
+        proceed.call(this, labels);
     });
 };
 
