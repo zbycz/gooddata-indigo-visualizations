fc281c1cf78a1e8225e970f9ce7616493876d14d
Tomáš Vojtášek <tomas.vojtasek@gooddata.com>
2017-09-05 11:05:06 +0200

    TRIVIAL: fix eslint

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,6 @@
 // Fail test on console error (react proptypes validation etc.)
-const consoleError = console.error;
-console.error = (err, ...args) => {
+const consoleError = console.error; // eslint-disable-line no-console
+console.error = (err, ...args) => { // eslint-disable-line no-console
     consoleError(err, ...args);
     throw new Error(err);
 };
